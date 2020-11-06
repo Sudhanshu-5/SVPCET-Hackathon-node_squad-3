60ec0368c3e7f53ad591c7465807d5535f077318
Rutuja2350 <65300153+Rutuja2350@users.noreply.github.com>
2020-11-06 19:14:15 +0530

    profile

---
+++ routes/profile1.js
@@ -0,0 +1,70 @@
+var express = require("express");
+var router = express.Router();   // new instance of the router
+var mongoose = require("mongoose");     
+var Profile = require("../models/Profile");
+var bodyParser = require('body-parser');
+// var middleware = require("../middleware"); 
+
+//get
+router.get("/", function(req, res){ 
+    Profile.find({}, function(err, allProfiles){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("profile",{profile: allProfiles, currentUser: req.user});
+        }
+    });
+});
+
+//post
+router.post("/", function(req, res){    
+    var year = req.body.year;
+    var lang = req.body.lang;
+    var tech = req.body.tech;
+    var comproject = req.body.comproject;
+    var comprojectdesc = req.body.comprojectdesc;
+    var incproject = req.body.incproject;
+    var incprojectdesc = req.body.incprojectdesc;
+    var codingplat = req.body.codingplat;
+    var failures = req.body.failures;
+   
+    var newProfile = {year: year, lang: lang, tech: tech, comproject: comproject, comprojectdesc: comprojectdesc, incproject: incproject, incprojectdesc: incprojectdesc, codingplat: codingplat, failures: failures};
+
+    Profile.create(newProfile, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect("/profile");  
+        }
+    });
+});
+
+// SHOW - shows more info about one campground
+router.get("/:id", function (req, res) {
+    //find the campground with provided ID
+    Profile.findById(req.params.id).exec(function (err, foundProfile) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("profile/showprofile", {profile: foundProfile});
+        }
+    });
+});
+
+
+// DESTROY CAMPGROUND ROUTE
+router.delete("/:id", middleware.checkProfileOwnership, function (req, res) {
+    Profile.findById(req.params.id, function (err, profile) {
+        if (err) {
+            res.redirect("/profile");
+        } else {
+            profile.remove();
+            res.redirect("/profile");
+        }
+    });
+});
+
+
+module.exports = router;
