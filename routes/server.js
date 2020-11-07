d1fda3e9440fd35b8944f8a0e1b46f08ef52b429
Rutuja2350 <65300153+Rutuja2350@users.noreply.github.com>
2020-11-07 11:49:15 +0530

    chat server route added

--- routes/server.js
+++ routes/server.js
@@ -1,40 +1,40 @@
 var express = require('express');
-var bodyParser = require('body-parser')
-var app = express();
-var http = require('http').Server(app);
+var router = express.Router();
+var bodyParser = require('body-parser');
+var http = require('http').Server(router);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
+var Message = require("../models/Message");
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+router.use(express.static(__dirname));
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: false}))
 
-var Message = mongoose.model('Message',{
-  name : String,
-  message : String
-})
+// var Message = mongoose.model('Message',{
+//   name : String,
+//   message : String
+// })
 
-var dbUrl = 'mongodb://username:password@ds257981.mlab.com:57981/simple-chat'
+// var dbUrl = 'mongodb://username:password@ds257981.mlab.com:57981/simple-chat'
 
-app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
+router.get("/messages", (req, res) => {
+  Message.find({}, function(err, message){
+      if(err){
+          console.log(err);
+      }
+      else{
+          res.render("./chat/index", {message: message});
+      }
   })
 })
 
-app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  })
-})
-
-app.post('/messages', (req, res) => {
+router.post('/messages', (req, res) => {
   var message = new Message(req.body);
   message.save((err) =>{
     if(err)
       sendStatus(500);
     io.emit('message', req.body);
-    res.sendStatus(200);
+    res.redirect("./chat/index");
   })
 })
 
@@ -42,10 +42,12 @@ io.on('connection', () =>{
   console.log('a user is connected')
 })
 
-mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
-  console.log('mongodb connected',err);
-})
+// mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
+//   console.log('mongodb connected',err);
+// })
 
-var server = http.listen(3001, () => {
-  console.log('server is running on port', server.address().port);
-});
+// var server = http.listen(3001, () => {
+//   console.log('server is running on port', server.address().port);
+// });
+
+module.exports = router;
