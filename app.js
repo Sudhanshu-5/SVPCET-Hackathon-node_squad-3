443aa578da7e71f55d7bd9c2ed69a32858a4ef75
Sudhanshu-5 <yawalkarsry@gmail.com>
2020-11-09 16:34:50 +0530

    final

--- app.js
+++ app.js
@@ -35,7 +35,6 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 //!mongo atlas connection String
-const mongoURI = "mongodb://sudhanshu:sudhanshu@cluster0-shard-00-00-7nbwd.mongodb.net:27017,cluster0-shard-00-01-7nbwd.mongodb.net:27017,cluster0-shard-00-02-7nbwd.mongodb.net:27017/helpmE?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 
 mongoose.connect(mongoURI, {
